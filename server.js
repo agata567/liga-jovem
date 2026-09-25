62f3cd6e585ab4f3ab6f9bcfe06ac890865bb577
agata567 <agata.silva.vieira@escola.pr.gov.br>
2026-09-25 07:50:35 -0300

    ok

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const db = mysql.createPool({
     host: 'localhost',      // Endereço do servidor MySQL
     port: 3306,             // Porta padrão do MySQL Workbench
     user: 'root',           // O teu utilizador do MySQL
-    password: 'A_TUA_SENHA', // <--- MUDAR AQUI: Coloca a tua palavra-passe do MySQL Workbench
+    password: '', // <--- MUDAR AQUI: Coloca a tua palavra-passe do MySQL Workbench
     database: 'banco_escola',
     waitForConnections: true,
     connectionLimit: 10,
